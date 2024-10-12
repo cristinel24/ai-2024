d92ad65a20cdf897d3dd57ff84ec1ddcb9a4ddb3
Rotariu George <rotariugeorge0@gmail.com>
2024-10-12 23:03:24 +0300

    add statistic logic and visualization

--- engine/statistics.py
+++ engine/statistics.py
@@ -1,5 +1,20 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def instances_per_class(df: pd.DataFrame) -> pd.DataFrame:
     return df["Race"].value_counts()
+
+def df_value_frequency(df: pd.DataFrame) -> list[(str, dict)]:
+    return [(attribute, df[attribute].value_counts().to_dict()) for attribute in df.columns]
+
+def plot_value_frequency(df: pd.DataFrame, attribute: str) -> None:
+    plt.hist(df[attribute], bins=50)
+    plt.xlabel(f'{attribute} classes')
+    plt.ylabel('Frequency')
+    plt.title(f'Value frequency in {attribute}')
+    plt.show()
+
+def plot_attributes_frequencies(df: pd.DataFrame) -> None:
+    for attribute in df.columns:
+        plot_value_frequency(df, attribute)
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from engine.constants import DATASET_PATH
 from engine.processing import process_dataset
-from engine.statistics import instances_per_class
+from engine.statistics import instances_per_class, df_value_frequency, plot_value_frequency, plot_attributes_frequencies
 
 if __name__ == "__main__":
     df = process_dataset(DATASET_PATH)
 
     print(instances_per_class(df))
     print(df.to_string())
+
+    value_frequencies = df_value_frequency(df)
+    for attribute_name, attribute_frequencies in value_frequencies:
+        print(f"Number of values for {attribute_name}: {len(attribute_frequencies)}")
+        print(attribute_frequencies)
+
+    plot_attributes_frequencies(df)
