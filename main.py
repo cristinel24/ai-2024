3383f054c5fbe8000fc76245bd5533dadbdff0f6
Cristian George ANDREI <crandrei@bitdefender.com>
2024-10-12 22:30:59 +0300

    transform non numeric + correlation plot

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from engine.constants import DATASET_PATH
 from engine.processing import process_dataset
 from engine.statistics import instances_per_class
+from engine.plots import df_correlation
+from engine.utils import transform_non_numeric
 
 if __name__ == "__main__":
     df = process_dataset(DATASET_PATH)
 
     print(instances_per_class(df))
     print(df.to_string())
+
+    transform_non_numeric(df)
+    df_correlation(df)
+
